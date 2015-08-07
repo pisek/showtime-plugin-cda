c480b9e4c5de77e34f32b4573d7179ba297c0b82
dkprd07 <dkprd07@jyskebank.dk>
2015-08-07 13:44:19 +0200

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# showtime-plugin-cda
+
+CDA.PL - Polish site with variety of content (mainly movies with polish dubbing and subtitles)
+
+For additional info, visit http://cda.pl
---
+++ cda/cda.js
@@ -0,0 +1,59 @@
+/**
+ *  cda plugin for Movian
+ *
+ *  Copyright (C) 2015 Pisek
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *  
+ */
+
+
+(function(plugin) {
+    var PREFIX = 'kisscartoon';
+    var logo = plugin.path + "logo.png";
+    
+    function setPageHeader(page, title) {
+        if (page.metadata) {
+            page.metadata.title = title;
+            page.metadata.logo = logo;
+        }
+        page.type = "directory";
+        page.contents = "items";
+    }
+
+	function d(c) {
+		print(JSON.stringify(c, null, 4));
+	}
+	
+    function browseItems(page, search) {
+        var pageNumber = 0;
+        page.entries = 0;
+
+        var url = 'http://kisscartoon.me/CartoonList/LatestUpdate';
+        d(url);
+        var c = showtime.httpReq(url);
+        d(c);
+        
+    }
+
+    plugin.addURI(PREFIX + ":start", function(page) {
+        setPageHeader(page, plugin.getDescriptor().synopsis);
+        
+        page.appendItem("", "separator", {
+            title: 'Newest'
+        });
+        browseItems(page);
+    });
+
+})(this);
