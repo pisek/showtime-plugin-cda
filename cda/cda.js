6585065f88e91d2f05cfb3f1edd66e3ed20ccf8e
Pisek <pisekfm@o2.pl>
2015-09-28 12:26:07 +0200

    background + default quality setting + fixed metadata

--- cda/cda.js
+++ cda/cda.js
@@ -20,22 +20,70 @@
 
 
 (function(plugin) {
-    var PREFIX = 'cda';
-    var logo = plugin.path + "logo.png";
+    var PREFIX = plugin.getDescriptor().id;
+    var LOGO = plugin.path + "logo.png";
+    var BACKGROUND = plugin.path + "views/img/background.png";
     
     var DEFAULT_URL = 'http://www.cda.pl/video/';
     
-    function setPageHeader(page, title) {
+	var service = plugin.createService(plugin.getDescriptor().title, PREFIX + ":start", "video", true, LOGO);
+	
+	var settings = plugin.createSettings(plugin.getDescriptor().title, LOGO, plugin.getDescriptor().synopsis);
+	
+	settings.createMultiOpt('quality', "Video quality (if not found - closest will be selected)", [
+        ['letMeChoose', 'Let me choose', true],
+        ['1080', '1080p'],
+        ['720', '720p'],
+        ['480', '480p'],
+        ['360', '360p'],
+        ['240', '240p']
+        ], function(v) {
+            service.quality = v;
+    	}
+    );
+    
+    function setPageHeader(page, title, image) {
         if (page.metadata) {
             page.metadata.title = title;
-            page.metadata.logo = logo;
+            page.metadata.logo = LOGO;
+            if (image) {
+            	page.metadata.background = image;
+            	page.metadata.backgroundAlpha = 0.3;
+            } else {
+            	page.metadata.background = BACKGROUND;
+            	page.metadata.backgroundAlpha = 0.7;
+            }
         }
     }
-    
-	var service = plugin.createService(plugin.getDescriptor().id, PREFIX + ":start", "video", true, logo);
 
 	function d(c) {
 		print(JSON.stringify(c, null, 4));
+	}
+	
+	function resolveDuration(h, m, s) {
+		if (h != null) {
+        	while(h.length != 2) {
+        		h = '0' + h;
+        	}
+        } else {
+        	h = '00';
+        }
+        if (m != null) {
+        	while(m.length != 2) {
+        		m = '0' + m;
+        	}
+        } else {
+        	m = '00';
+        }
+        if (s != null) {
+        	while(s.length != 2) {
+        		s = '0' + s;
+        	}
+        } else {
+        	s = '00';
+        }
+        	
+        return h + ':' + m + ':' + s;
 	}
 	
     function browseItems(page, search) {
@@ -126,90 +174,124 @@
     	page.loading = true;
     	page.type = "directory";
         page.contents = "items";
+        setPageHeader(page, "Searching...", image);
         
         d(DEFAULT_URL + id);
         var c = showtime.httpReq(DEFAULT_URL + id);
         
-        // 1 - type, 2 - description, 3 - title, 4 - imageurl, 5 - negative-rating
-        var pattern = /<meta property="og:type" content="(.*?)".*?>[\s\S]*?<meta property="og:description" content="([\s\S]+?)".*?>\s*?<meta property="og:title" content="(.+?)".*?>\s*?<meta property="og:image" content="(.+?)".*?>[\s\S]*?<span class="bialeSred"><span class="szareSred" style="width:(\d*?)px"><\/span><\/span>/igm;
-        if ((match = pattern.exec(c)) !== null) {
-        	d(match[1]);
-        	d(match[2]);
-        	d(match[3]);
-        	d(match[4]);
-        	d(match[5]);
-        	d(match[6]);
-        	var type = match[1];
-        	var desc = match[2];
-        	var title = match[3];
-        	var image = match[4];
-        	var rating = (80-match[6])/80*100;
-        	
-        	page.metadata.title = title;
-        	page.metadata.background = image;
-        	page.metadata.backgroundAlpha = 0.3;
-        }
+        //show screen only if letMeChoose quality
+        if (service.quality == 'letMeChoose') {
         
+	        // 1 - type, 2 - description, 3 - title, 4 - imageurl, 5 - negative-rating
+	        var pattern = /<meta property="og:type" content="(.*?)".*?>[\s\S]*?<meta property="og:description" content="([\s\S]+?)".*?>\s*?<meta property="og:title" content="(.+?)".*?>\s*?<meta property="og:image" content="(.+?)".*?>[\s\S]*?<span class="bialeSred"><span class="szareSred" style="width:(\d*?)px"><\/span><\/span>/igm;
+	        if ((match = pattern.exec(c)) !== null) {
+	        	d(match[1]);
+	        	d(match[2]);
+	        	d(match[3]);
+	        	d(match[4]);
+	        	d(match[5]);
+	        	var type = match[1];
+	        	var desc = match[2];
+	        	var title = match[3];
+	        	var image = match[4];
+	        	var rating = (80-match[5])/80*100;
+	        	d(rating);
+	        	
+	        	setPageHeader(page, title, image);
+	        }
+	        
+	        
+	        // 1 - duration
+	        var pattern = /<meta itemprop='duration' content='T((\d*?)H)*((\d*)?M)*(\d+?)S' \/>/igm;
+	        if ((match = pattern.exec(c)) !== null) {
+	        	d(match);
+	        	var h = match[2];
+	        	var m = match[4];
+	        	var s = match[5];
+	        	
+	        	var duration = resolveDuration(h, m, s);
+	        	d(duration);
+	        }
+	        
+	        page.appendItem("", "separator", {
+	            	title: "Quality"
+	        });
         
-        // 1 - duration
-        var pattern = /<meta itemprop='duration' content='T((\d*?)H)*((\d*)?M)*(\d+?)S' \/>/igm;
-        if ((match = pattern.exec(c)) !== null) {
-        	d(match);
-        	var h = match[2];
-        	var m = match[4];
-        	var s = match[5];
-        	
-        	if (h != null) {
-        		while(h.length != 2) {
-        			h = '0' + h;
-        		}
-        	} else {
-        		h = '00';
-        	}
-        	if (m != null) {
-        		while(h.length != 2) {
-        			m = '0' + m;
-        		}
-        	} else {
-        		m = '00';
-        	}
-        	if (s != null) {
-        		while(s.length != 2) {
-        			s = '0' + s;
-        		}
-        	} else {
-        		s = '00';
-        	}
-        	
-        	var duration = h + ':' + m + ':' + s;
-        	d(duration);
-        }
+    	}
         
-        page.appendItem("", "separator", {
-            	title: "Quality"
-        });
-        
-        // 1 - link url, 2 - quality
-        var pattern = /<a.*?href="\/video\/(\d\w+\?wersja=(\d\w+))".*?>/igm;
+        // 1 - url, 2 - id, 3 - quality
+        var pattern = /<a.*?href="\/video\/((\d\w+)\?wersja=(\d\w+)p)".*?>/igm;
         var addedQuality = false;
+        var bestMatch;
         while ((match = pattern.exec(c)) !== null) {
+        	match[3] = parseInt(match[3]);
         	d(match);
-        	page.appendItem(PREFIX + ":video:" + match[1], 'video', {
-						title : match[2],
+        	var id = match[2];
+        	var quality = match[3];
+        	var url = match[1];
+        	addedQuality = true;
+        	
+        	if (service.quality == 'letMeChoose') {
+        	
+	        	page.appendItem(PREFIX + ":video:" + id + ":" + quality + ":" + url, 'video', {
+						title : quality+"p",
 						icon : image,
 						genre: type,
 						rating: rating,
 						duration: duration,
 						description : desc
 					});
-			addedQuality = true;
+					
+            } else {
+            	
+            	var desiredQuality = parseInt(service.quality);
+            	
+            	if (desiredQuality == quality) {	//find desired quality
+            		page.redirect(PREFIX+":video:" + id + ":" + quality + ":" + url);
+            	}
+            	
+            	// init
+            	if (bestMatch == null) {
+            		bestMatch = match;
+            		continue;
+            	}
+            	
+	        	//find closest quality from bottom (bottom first)
+		        if (quality > bestMatch[3] && quality < desiredQuality) {
+					bestMatch = match;
+					d(bestMatch);
+					continue;
+				}
+				
+		        //find closest quality from top
+		        if (quality < bestMatch[3] && quality > desiredQuality) {
+					bestMatch = match;
+					d(bestMatch);
+					continue;
+				}
+		        
+            }
+        	
         }
         
+		d(bestMatch);
         
-        
-        //if there are no quality versions, add a default one
-        if (!addedQuality) {
-        	page.appendItem(PREFIX + ":video:" + id, 'video', {
+        if (service.quality != 'letMeChoose') {
+   	        //if there are no quality versions, show a default one
+	        if (!addedQuality) {
+				page.redirect(PREFIX + ":video:" + id + ":480:" + id);
+	        } else {
+	        	if (bestMatch == null) {
+			       	page.error("Selected video is not available on this platform.");
+					return;
+			    } else {
+		        	page.redirect(PREFIX+":video:" + bestMatch[2] + ":" + bestMatch[3] + ":" + bestMatch[1]);
+			    }
+	        }
+        } else {
+   	        //if there are no quality versions, run a default one
+	        if (!addedQuality) {
+	        	page.appendItem(PREFIX + ":video:" + id + ":480:" + id, 'video', {
 						title : "Default quality",
 						icon : image,
 						genre: type,
@@ -217,34 +299,54 @@
 						duration: duration,
 						description : desc
 					});
+	        }
         }
         
         page.loading = false;
+        
     });
     
-    plugin.addURI(PREFIX + ":video:(.*)", function(page, id) {
+    plugin.addURI(PREFIX + ":video:(.*):(.*):(.*)", function(page, id, quality, url) {
     	page.loading = true;
+    	var videoUrl;
+    	var title;
+    	var metadata = {};
 
-        var c = showtime.httpReq(DEFAULT_URL + id);
-        d(c.headers);
+        var c = showtime.httpReq(DEFAULT_URL + url);
+        //d(c.headers);
         
+        // 1 - movie url
         var pattern = /if \(checkFlash\(\)\)\{\s*l='(.*)';\s*jwplayer/igm;
         if ((match = pattern.exec(c)) !== null) {
         	/*c = showtime.httpReq(match[1]);
         	d(c.headers);*/
         	d(match[1]);
-        	page.source = match[1];
+        	videoUrl = match[1];
         } else {
         	//youtube movie (or other)
         	page.error("Cannot open links from other sites (like youtube etc).");
         	d('cannot open movie other than cda');
         	return;
         }
+        
+        // 1 - title
+        var pattern = /<meta property="og:title" content="(.+?)".*?>/igm;
+        if ((match = pattern.exec(c)) !== null) {
+        	title = match[1];
+        }
+        
+        metadata.title = title;
+        metadata.sources = [{ url: videoUrl, bitrate: quality }];
+        metadata.canonicalUrl = PREFIX + ":movie:" + id;
+        metadata.no_fs_scan = true;
+        d(metadata);
+        setPageHeader(page, title);
         page.loading = false;
+        page.source = "videoparams:"+showtime.JSONEncode(metadata);
         page.type = "video";
     });
     
-	plugin.addSearcher(plugin.getDescriptor().id, logo, function(page, search) {
+	plugin.addSearcher(plugin.getDescriptor().id, LOGO, function(page, search) {
         browseItems(page, search);
     });
 
